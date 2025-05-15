cc65f43ea4ffb541a0cd564693eed5eb35f4d1fd
Osezele Oghedegbe <oghedegbeosezele@gmail.com>
2025-05-15 15:55:40 +0100

    project initalized

---
+++ src/main/java/com/zele/crspringboot/CrSpringBootApplication.java
@@ -0,0 +1,12 @@
+package com.zele.crspringboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CrSpringBootApplication {
+    public static void main(String[] args) {
+        SpringApplication.run(CrSpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/zele/crspringboot/CrSpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zele.crspringboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CrSpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
