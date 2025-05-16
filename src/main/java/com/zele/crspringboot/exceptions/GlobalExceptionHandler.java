9236c2d3eab36a7b901ff09cb79853b8ccd1c5ba
Osezele Oghedegbe <oghedegbeosezele@gmail.com>
2025-05-16 19:14:03 +0100

    feat: add global exception handler

---
+++ src/main/java/com/zele/crspringboot/exceptions/GlobalExceptionHandler.java
@@ -0,0 +1,25 @@
+package com.zele.crspringboot.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+
+@ControllerAdvice
+public class GlobalExceptionHandler {
+
+    @ExceptionHandler(EntityAlreadyExistsException.class)
+    public ResponseEntity<String> handleEntityAlreadyExists(EntityAlreadyExistsException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.CONFLICT);
+    }
+
+    @ExceptionHandler(EntityNotFoundException.class)
+    public ResponseEntity<String> handleEntityNotFound(EntityNotFoundException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.NOT_FOUND);
+    }
+
+    @ExceptionHandler(EntityNotAuthorizedException.class)
+    public ResponseEntity<String> handleEntityNotAuthorized(EntityNotAuthorizedException ex) {
+        return new ResponseEntity<>(ex.getMessage(), HttpStatus.UNAUTHORIZED);
+    }
+}
