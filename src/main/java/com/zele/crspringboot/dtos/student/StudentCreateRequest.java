cbbbb05bba15a591dfef4d33354d65e9af6c194f
Osezele Oghedegbe <oghedegbeosezele@gmail.com>
2025-05-16 13:56:47 +0100

    feat: create student dtos

---
+++ src/main/java/com/zele/crspringboot/dtos/student/StudentCreateRequest.java
@@ -0,0 +1,18 @@
+package com.zele.crspringboot.dtos.student;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class StudentCreateRequest {
+    private String firstName;
+    private String lastName;
+    private String email;
+}
---
+++ src/main/java/com/zele/crspringboot/dtos/student/StudentViewDTO.java
@@ -0,0 +1,19 @@
+package com.zele.crspringboot.dtos.student;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonInclude;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@JsonInclude(JsonInclude.Include.NON_NULL)
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class StudentViewDTO {
+    private Long id;
+    private String firstName;
+    private String lastName;
+    private String email;
+}
